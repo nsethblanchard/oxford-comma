03a54df8be704e1fea6be8cf34d2537eb08e221d
Seth Blanchard <nsethblanchard@gmail.com>
2020-10-02 13:27:19 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array[1] == nil
+        array.join
+    elsif array[1] != nil && array[2] == nil
+        array.join(" and ")
+    elsif array[2] != nil && array[3] == nil
+        new_array = array[0..1].join(", ") 
+        new_array + ", and #{array[2]}"
+    else array[3] != nil
+        new_array2 = array[0..-2].join(", ")
+        new_array2 + ", and #{array[-1]}"
+    end
+    
 end
